2c163566f15008b8b588d8cfbf1509889c8f6102
pho <phonum5@gmail.com>
2022-08-06 22:41:37 +0000

    added feature to make navbar transparent while scrolling

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-let menu = document.querySelector('#icon')
-let navbar = document.querySelector('.navbar')
-let hiddenLinks = document.querySelector('.navbar-bottom')
+let menu = document.querySelector("#icon");
+let navbar = document.querySelector(".navbar");
+let hiddenLinks = document.querySelector(".navbar-bottom");
 
-menu.addEventListener('click', ()=>{
-    hiddenLinks.classList.toggle('bottom-nav-shown')
-    hiddenLinks.classList.toggle('light-gray')
-    navbar.classList.toggle('light-gray')
-})
+menu.addEventListener("click", () => {
+  hiddenLinks.classList.toggle("bottom-nav-shown");
+  hiddenLinks.classList.toggle("light-gray");
+  navbar.classList.toggle("light-gray");
+});
+window.addEventListener("scroll", function () {
+  //   const scrollTop = window.scrollY;
+  //   const height = sticky.offsetHeight;
+  //   const offset = height / 2;
+  //   let calc = 1 - (scrollTop - offset + range) / range;
+
+  //   if (calc > 1) {
+  //     navbar.style.opacity = 0;
+  //   } else if (calc < 0) {
+  //     navbar.style.opacity = 0.5;
+  //   }
+
+  if (window.scrollY >= navbar.offsetHeight) {
+    navbar.style.opacity = "0.5";
+  }
+  if (window.scrollY >= navbar.offsetHeight * 2) {
+    navbar.style.opacity = "0";
+  }
+  if (window.scrollY < navbar.offsetHeight) {
+    navbar.style.opacity = "1";
+  }
+});
